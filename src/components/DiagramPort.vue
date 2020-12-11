8a4020d0abb6df6f1cce293db1f4a81e300f6cd5
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2020-12-11 02:10:43 +0000

    feat(diagramport): add `input` port hear
    To match ork done on `Out` ports

--- src/components/DiagramPort.vue
+++ src/components/DiagramPort.vue
@@ -1,68 +1,76 @@
 <template>
   <g>
     <svg :y="y + 55" v-if="type === 'in'">
-      <rect
-        :fill="fill"
-        ref="handle"
-        x="0" y="1"
-        rx="3" ry="3"
-        width="10" height="10"
-        @mouseenter="enter" @mouseleave="leave" @mousedown="startDragNewLink" @mouseup="mouseup">
-      </rect>
-      <text x="12" y="9" font-size="8pt" fill="#000000">{{name}}</text>
-    </svg>
-    <svg :y="y + 55" v-else>
-      <!-- <rect
-        :fill="fill"
-        ref="handle_OLD"
-        :x="nodeWidth" y="1"
-        rx="3" ry="3"
-        width="10" height="10"
-        @mouseenter="enter" @mouseleave="leave" @mousedown="startDragNewLink" @mouseup="mouseup">
-      </rect> -->
-      <!-- <path opacity="0.8" fill="#642765" stroke="none"
-        :d="`
-          M ${nodeWidth - (name.length * fontSize + 10)} 1
-          a 5 5 0 0 0 -5 5
-          a 5 5 0 0 0 5 5
-          h ${name.length * fontSize + 10}
-          v -10
-          h ${(name.length * fontSize + 10) * -1}
-          z `" />
-      <path opacity="1" fill="#b0b0ff" stroke="#000000"
-        :d="`
-          M ${nodeWidth - 10} 1
-          a 5 5 0 0 0 -5 5
-          a 5 5 0 0 0 5 5
-          h 10
-          v -10
-          h -10
-          z `" /> -->
       <path opacity="0.8" fill="#642765" stroke="none" @click="configurePort" style="cursor: pointer"
+        filter="url(#filter_gaus_20)"
         :d="`
-          M ${nodeWidth - (name.length * fontSize + 5)} 0
-          a 9 9 0 0 0 -9 9
-          a 9 9 0 0 0 9 9
+          M 5 0
+          v 19
           h ${name.length * fontSize + 10}
-          v -18
+          a 9.5 9.5 0 0 0 0 -19
           h ${(name.length * fontSize + 10) * -1}
           z `" />
-      <path opacity="1" stroke="#000000" stroke-width="1"
+      <path opacity=".6" stroke="none"
+        filter="url(#filter_gaus_20)"
+        fill="#00000"
+        :d="`
+          M 5 0
+          v 19
+          h 10
+          a 9.5 9.5 0 0 0 0 -19
+          h -10
+          z `" />
+      <path opacity="1" stroke="none"
         ref="handle"
+         style="cursor: crosshair"
         :fill="fill"
         @mouseenter="enter" @mouseleave="leave" @mousedown="startDragNewLink" @mouseup="mouseup"
         :d="`
-          M ${nodeWidth - 5} 1
-          a 8 8 0 0 0 -8 8
-          a 8 8 0 0 0 8 8
+          M 5 0.5
+          v 18
           h 10
-          v -16
+          a 9 -9 0 0 0 0 -18
+          h -10
+          z `" />
+      <text :x="30" :y="9 + fontSize / 2" :font-size="`${fontSize}pt`" fill="#FFFFFF" @click="configurePort" style="cursor: pointer">{{name}}</text>
+      <text :x="10" :y="9 + fontSize / 2" :font-size="`${fontSize}pt`" fill="#000000" style="cursor: crosshair" @mouseenter="enter" @mouseleave="leave" @mousedown="startDragNewLink" @mouseup="mouseup">{{ connectorCategory }}</text>
+    </svg>
+    <svg :y="y + 55" v-else>
+      <path opacity="0.8" fill="#642765" stroke="none" @click="configurePort" style="cursor: pointer"
+        filter="url(#filter_gaus_20)"
+        :d="`
+          M ${nodeWidth - (name.length * fontSize + 5)} 0
+          a 9.5 9.5 0 0 0 0 19
+          h ${name.length * fontSize + 10}
+          v -19
+          h ${(name.length * fontSize + 10) * -1}
+          z `" />
+      <path opacity=".6" stroke="none"
+        filter="url(#filter_gaus_20)"
+        fill="#00000"
+        :d="`
+          M ${nodeWidth - 5} 0
+          a 9.5 9.5 0 0 0 0 19
+          h 10
+          v -19
+          h -10
+          z `" />
+      <path opacity="1" stroke="none"
+        ref="handle"
+         style="cursor: crosshair"
+        :fill="fill"
+        @mouseenter="enter" @mouseleave="leave" @mousedown="startDragNewLink" @mouseup="mouseup"
+        :d="`
+          M ${nodeWidth - 5} 0.5
+          a 9 9 0 0 0 0 18
+          h 10
+          v -18
           h -10
           z `" />
       <!-- <text :x="12" y="9" font-size="8pt" fill="#000000">{{name}}</text> -->
       <text :x="nodeWidth - (name.length * fontSize + 0)" :y="9 + fontSize / 2" :font-size="`${fontSize}pt`" fill="#FFFFFF" @click="configurePort" style="cursor: pointer">{{name}}</text>
       <!-- <text :x="nodeWidth - 24" y="9" font-size="8pt" fill="#000000" @click="configurePort" style="cursor: pointer">o</text> -->
-      <text :x="nodeWidth - 7" :y="9 + fontSize / 2" :font-size="`${fontSize}pt`" fill="#000000" @mouseenter="enter" @mouseleave="leave" @mousedown="startDragNewLink" @mouseup="mouseup">{{ connectorCategory }}</text>
+      <text :x="nodeWidth - 7" :y="9 + fontSize / 2" :font-size="`${fontSize}pt`" fill="#000000" style="cursor: crosshair" @mouseenter="enter" @mouseleave="leave" @mousedown="startDragNewLink" @mouseup="mouseup">{{ connectorCategory }}</text>
       <!-- <text :x="nodeWidth - 12" y="9" font-size="8pt" fill="#000000" @click="deletePort" style="cursor: pointer">x</text> -->
     </svg>
   </g>
