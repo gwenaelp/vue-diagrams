771bda89438794b812d65cd279bbae3cd52f513c
Gwenael Pluchon <accounts@gwenp.fr>
2023-03-14 22:43:15 +0100

    add roboto font on doc page

--- doc/layouts/default.vue
+++ doc/layouts/default.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
-    Vue-diagrams header
+    <div class="header">
+      <div class="container">
+        <a href="#/" class="logo">
+          Vue-diagrams
+        </a>
+      </div>
+    </div>
     <slot/>
   </div>
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap');
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
+.container {
+  max-width: 960px;
+  margin: auto;
+}
+
+</style>
+<style scoped>
+.logo {
+  font-size: 36px;
+  font-weight: lighter;
+}
+</style>
