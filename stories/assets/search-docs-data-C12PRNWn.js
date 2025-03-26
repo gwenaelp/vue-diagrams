4ffd5da8a9bf6119b70b32746519996f56785d75
gwenaelp <pro+github@gwenp.fr>
2025-03-26 20:53:45 +0000

    Deploying to gh-pages from  @ 138038a10fa6e438790152dcdcc875cac238e516 🚀

--- stories/assets/search-docs-data-C12PRNWn.js
+++
@@ -1,9 +0,0 @@
-import { W as markRaw } from "./vendor-BvBmhImZ.js";
-let searchData$1 = { "index": [], "idMap": {} };
-const searchData = markRaw(searchData$1);
-function onUpdate(cb) {
-}
-export {
-  onUpdate,
-  searchData
-};
